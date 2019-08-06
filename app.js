80ff6ddad33f4d2ba4a9018d2daae65ac7eb131c
navin.kiit.nk@gmail.com <navin.kiit.nk@gmail.com>
2019-08-06 10:26:33 +0530

    okey

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// server.js
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -5,11 +7,7 @@ const path = require("path");
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
 app.listen(process.env.PORT || 4000, function() {
   console.log("Your node js server is running");
 });
-
-//Run app, then load http://localhost:port in a browser to see the output.
