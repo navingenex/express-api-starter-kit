ca932252add459c9fbfa2dd59885b178c36fded2
navin.kiit.nk@gmail.com <navin.kiit.nk@gmail.com>
2019-08-06 10:51:37 +0530

    okey

--- app.js
+++ app.js
@@ -3,14 +3,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+app.get("/api/getData", (req, res) => {
+  res.send({ hello: "hello" });
+  res.end();
+});
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
-});
-app.get("/api/getData", (req, res) => {
-  res.send({
-    hello: "data"
-  });
 });
 
 app.listen(process.env.PORT || 4000, function() {
